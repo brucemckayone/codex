99111b158902a816806f9a629111a587723c2f58
brucemckay <brucemckayone@gmail.com>
2025-10-29 11:56:06 +0000

    Preventing .env.dev from being used in integration testing in the IC pipeline.

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -4,7 +4,10 @@ import { resolve } from 'path';
 import { DbEnvConfig } from '@codex/database/config/env.config';
 
 // Load env.dev from project root (2 levels up from packages/database)
-config({ path: resolve(__dirname, DbEnvConfig.rootEnvPath) });
+
+// check against a random env variable, which is defined in CI only load env.dev if no env vars defined
+if (!DbEnvConfig.method)
+  config({ path: resolve(__dirname, DbEnvConfig.rootEnvPath) });
 
 export default defineConfig({
   out: DbEnvConfig.out,
